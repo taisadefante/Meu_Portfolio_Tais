8b59da59f2d788bb5363eab865f57112679c9547
tais defante <taisadefante@hotmail.com>
2023-06-30 20:18:41 -0300

    nova pasta

---
+++ Js/functions.js
@@ -0,0 +1,16 @@
+
+
+/* ou     $(document).ready(funcion(){     
+
+});*/
+
+/* aparecer so apos toda pagina carregar - carregar script
+
+$(window).on('load', function()){
+
+});*/
+
+
+$(function(){
+    alert("ola undi")
+});
