7a5978847ad084d72d04cd1273ec55554f1320e5
tais defante <taisadefante@hotmail.com>
2023-08-25 11:16:22 -0300

    retirada certificados

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-  cssMode: true,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
-});
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
