e1ba3d5f8f43d4c65012b33b696452df884aaa6d
tais defante <taisadefante@hotmail.com>
2023-12-27 23:30:25 -0300

    novo

--- script.js
+++ script.js
@@ -1,82 +1,122 @@
- var typed = new Typed(".typing", {
-    strings: [ "Desenvolvedora Full Stack", "Desenvolvedora Web", "Desenvolvedora de Software", "Frond-End", "Data Analytics", "Gerente de Projetos", "Infraestrutura de TI","Administradora"], 
-    typespeed: 100, 
-    Backspeed: 60, 
-    loop: true
- });
+const wrapper = document.querySelector(".wrapper ");
+const carousel = document.querySelector(".carousel ");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i ");
+const carouselChildrens = [...carousel.children];
 
- const nav = document.querySelector(".nav"),
- navList = nav.querySelectorAll("li"),
- totalNavList = navList.length,
- allSection = document.querySelectorAll(".section"),
- totalSection = allSection.length;
- for(let i = 0; i < totalNavList; i++) {
-   const a = navList[i].querySelector("a");
-   a.addEventListener("click", function() {
-      removeBackSection();
-      for(let j = 0; j < totalNavList; j++) {
-         if(navList[j].querySelector("a").classList.contains("active")) {
-            addBackSection(j);
-           // allSection[j].classList.add("back-section");
-         }
-         navList[j].querySelector("a").classList.remove("active");
-      }
-      this.classList.add("active");
-      showSection(this);
-      if(window.innerWidth < 1200) {
-         asideSectionTogglerBtn();
-      }
-   })
- }
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
 
- function removeBackSection() {
-   for(let i = 0; i < totalSection; i++) {
-      allSection[i].classList.remove("back=section");
-   }
- }
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
- function addBackSection(num) {
-   allSection[num].classList.add("back-section")
- }
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
 
- function showSection(element) {
-   for(let i = 0; i < totalSection; i++) {
-      allSection[i].classList.remove("active");
-   }
-   const target = element.getAttribute("href").split("#")[1];
-   document.querySelector("#" + target).classList.add("active");
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
 
- }
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
 
- function updateNav(element) {
-   for(let i = 0; i < totalNavList; i++) {
-      navList[i].querySelector("a").classList.remove("active");
-      const target = element.getAttribute("href").split("#")[1];
-      if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
-         navList[i].querySelector("a").classList.add("active");
-      }
-   }
- }
- document.querySelector(".hire-me").addEventListener("click", function () {
-   const sectionIndex = this.getAttribute("data-section-index");
-   showSection(this);
-   updateNav(this);
-   removeBackSection();
-   addBackSection(sectionIndex);
- })
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
 
- const navTogglerBtn = document.querySelector(".nav-toggler"),
- aside = document.querySelector(".aside");
- navTogglerBtn.addEventListener("click", () => {
-   asideSectionTogglerBtn();
- })
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
 
- function asideSectionTogglerBtn() {
-   aside.classList.toggle("open");
-   navTogglerBtn.classList.toggle("open");
-   for(let i = 0; i < totalSection; i++) {
-      allSection[i].classList.toggle("open");
-   }
- }
+const dragging = (e) => {
+  if (!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
 
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
 
+const infiniteScroll = () => {
+  // If the carousel is at the beginning, scroll to the end
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+  // If the carousel is at the end, scroll to the beginning
+  else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+
+  // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+};
+
+const autoPlay = () => {
+  if (window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+  // Autoplay the carousel after every 2500 ms
+  timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 2500);
+};
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+
+//****************************************************************** */
+
+//****************************************************************** */
+
+const backToTopButton = document.querySelector("a#backToTopButton");
+
+window.addEventListener("scroll", showBackToTopButtonOnScroll);
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 300) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "100px",
+  duration: 2200,
+}).reveal(`
+.title,
+.card,
+#knowledge ul li,
+#contact a,
+#contact input,
+#contact textarea`);
